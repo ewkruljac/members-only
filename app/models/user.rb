e29907b51e0c782898800a57384b13ad7b9e2d84
Edward Kruljac <ewkruljac@gmail.com>
2016-10-07 22:22:14 -0600

    Added tests for user models, indexed email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
 
+  before_save { email.downcase! }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,     presence: true
+  validates :password, presence: true, 
+                       length: { minimum: 6 }
+  validates :email,    presence: true,
+                       format: { with: VALID_EMAIL_REGEX },
+                       uniqueness: { case_sensitive: false }
+
   has_secure_password
 end
